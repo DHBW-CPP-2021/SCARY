3c1cb39a1f1bc6aa4557945359917b95ee04389c
Patsch36 <scheichpatrick@gmail.com>
2021-07-22 14:18:28 +0200

    Fixed a linking issue

--- LabyrinthGameLib/Game.h
+++ LabyrinthGameLib/Game.h
@@ -15,15 +15,22 @@ namespace LabyrinthGame
         Game();
         void run();
     private:
+        // Config Methods
         void config();
         bool createBoard();
         bool createPlayers();
         bool createGameRules();
         bool gameOver();
+
+        // round methods
         void round();
+        void LabyrinthGame::Game::playermove();
+
+        // members
         std::vector<std::shared_ptr<AbstractPlayer>> m_players;
         std::vector<std::shared_ptr<TreasureToken>> m_treasures;
         static std::shared_ptr<GameRules> m_rules;
         static std::shared_ptr<GameBoard> m_board; // static???
+
     };
 } // namespace LabyrinthGame
