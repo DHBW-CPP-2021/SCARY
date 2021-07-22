b9ffe57e9d8d21c0f949747c099ca2c11a25d248
SB0599 <sbaspach.01@gmail.com>
2021-07-22 15:55:13 +0200

    checkMove and checkPiceMove implemented

--- LabyrinthGameLib/GameRules.cpp
+++ LabyrinthGameLib/GameRules.cpp
@@ -4,20 +4,25 @@
 LabyrinthGame::GameRules::GameRules(std::vector<std::weak_ptr<AbstractPlayer>> players, std::weak_ptr<GameBoard> board)
     : m_board(board), m_players(players)
 {
-
 }
 
 bool LabyrinthGame::GameRules::checkMove(std::weak_ptr<AbstractPlayer> player,
                                          const LabyrinthGame::GameBoard::Coordinate move)
 {
-
-    return false;
+    std::shared_ptr<AbstractPlayer> f_player = player.lock();
+    std::shared_ptr<GameBoard> f_board = m_board.lock();
+    return f_board->coordsAreConnected(move, f_player->getCoordinate());
 }
 
 bool LabyrinthGame::GameRules::checkPieceMove(const Coordinate &coordinate)
 {
     // with Maze?  .isColumFixed() or isRawFixed()  in Board implemented ??
-    return false;
+    std::shared_ptr<GameBoard> f_board = m_board.lock();
+    Maze maze = f_board->getMaze();
+    if (maze.isFixed(coordinate))
+        return false;
+    else
+        return true;
 }
 
 bool LabyrinthGame::GameRules::checkWin(std::weak_ptr<AbstractPlayer> player)
@@ -33,8 +38,8 @@ bool LabyrinthGame::GameRules::checkWin(std::weak_ptr<AbstractPlayer> player)
 bool LabyrinthGame::GameRules::winPosition(std::weak_ptr<AbstractPlayer> player)
 {
     std::shared_ptr<AbstractPlayer> f_player = player.lock();
-    std::shared_ptr<GameBoard> board = m_board.lock();
-    if (board->coordIsConnectedToOutside(f_player->getCoordinate()))
+    std::shared_ptr<GameBoard> f_board = m_board.lock();
+    if (f_board->coordIsConnectedToOutside(f_player->getCoordinate()))
     {
         return true;
     }
